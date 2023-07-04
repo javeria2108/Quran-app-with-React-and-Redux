e96537c1f26553cd04c812f05c09620b18eb488c
javeria2108 <javeriazh@gmail.com>
2023-07-04 17:41:21 +0500

    home card

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import Search from './Search'
+import { useSelector,useDispatch } from 'react-redux';
+import { fetchChapters } from '../redux/thunk';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const chapters = useSelector((state) => state.chapters.data);
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(fetchChapters());
+
+  },[dispatch])
   return (
     <div className=''>
+    
         <Search/>
+        <div className='flex flex-wrap'>
+{chapters.map((chapter)=>{
+            return(
+              
+                <div className='box-content w-56 h-20 m-5 border-2
+                 p-5 text-white '  key={chapter.id}>
+        <div className='flex flex-row justify-between'>
+                  <p>{chapter.name_simple}</p>
+                  <p>{chapter.name_arabic}</p>
+                  </div>
+                  <div>
+                    <p>{'verses: '+ chapter.verses_count}</p>
+                    <p>{'Revelead in: '+ chapter.revelation_place}</p>
+
+                    <p>{chapter.translated_name.name}</p>
+                  </div>
+
+                   
+                </div>
+            );
+
+        })}
+    </div>
         </div>
+
   )
 }
 
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -24,7 +24,8 @@ console.log(chapters);
   return (
     <div className='flex flex-col items-center max-w-md mx-auto '>
       <input
-      className=' mt-20 mb-3 w-80 px-8 py-3 rounded-full bg-yellow-l text-dark-maroon placeholder-dark-maroon' 
+      className=' mt-20 mb-3 w-80 px-8 py-3 rounded-full bg-yellow-l text-dark-maroon
+       placeholder-dark-maroon focus: ring-dark-maroon' 
        type="text"
         placeholder='What do you want to read?'
        onChange={inputHandler}
