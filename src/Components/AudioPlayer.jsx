ff547fce51820a87398ba4c8cbedabf0c45c5cef
javeria2108 <javeriazh@gmail.com>
2023-07-06 15:54:53 +0500

    audio controls anhanced

--- src/Components/AudioPlayer.jsx
+++ src/Components/AudioPlayer.jsx
@@ -4,7 +4,7 @@ import { fetchAudioUrl } from "../redux/thunk/fetchAudio";
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay,faPause,faForwardStep, faBackwardStep } from "@fortawesome/free-solid-svg-icons";
-
+import { fetchChapters } from "../redux/thunk";
 
 
 const play=<FontAwesomeIcon icon={faPlay}/>
@@ -12,9 +12,11 @@ const pause=<FontAwesomeIcon icon={faPause}/>
 const forward=<FontAwesomeIcon icon={faForwardStep}/>
 const backward=<FontAwesomeIcon icon={faBackwardStep}/>
 export function QuranAudioPlayer() {
-  const { id } = useParams();
+  const { id : idParam} = useParams();
+  const[id,setId]=useState(parseInt(idParam))
   const dispatchAudio = useDispatch();
   const audioUrl = useSelector((state)=>state.audio.audioUrl);
+  const chapters = useSelector((state) => state.chapters.data);
   const [isPlaying, setPlaying] = useState(false);
     const audioRef = useRef();
     useEffect(() => {
@@ -28,23 +30,53 @@ export function QuranAudioPlayer() {
     }
   }, [isPlaying]);
 
+  
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchChapters());
+  }, [dispatch,id]);
   const togglePlayPause = () => {
     setPlaying(!isPlaying);
   };
 
-
+const handleForward=()=>{
+  if (id < 114) {
+    setId(id + 1);
+    setPlaying(!isPlaying)
+  }
+  }
+const handleBackward=()=>{
+  if (id > 1) {
+    setId(id - 1);
+    setPlaying(!isPlaying)
+  }
+}
+const chapterInfo=chapters[id-1];
+let chapterNameSimple, chapterNameArabic;
+if (chapterInfo) {
+  chapterNameSimple = chapterInfo.name_simple;
+  chapterNameArabic = chapterInfo.name_arabic;
+}
 
     return (
-      <div className="flex justify-center items-center h-screen bg-gradient-to-b from-cyan-700 to-slate-950 text-white">
+      <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-cyan-700 to-slate-950 text-white">
      <div className="w-full sm:w-11/12 md:w-10/12 lg:w-8/12 h-3/4 sm:h-11/12 md:h-10/12 lg:h-3/4 bg-black bg-opacity-50 rounded shadow p-5">
       <audio ref={audioRef} src={audioUrl} preload="metadata">
         Your browser does not support the audio element.
       </audio>
+
+      <div className="mt-20 flex flex-col justify-center items-center">
+<h1 className="p-4 text-2xl">
+  
+</h1>
+<h1 className="text-4xl">{chapterNameSimple}</h1>
+<h1 className="text-3xl">{chapterNameArabic}</h1>
+      </div>
       
-      <div className="flex flex-row mx-5 py-1 sm:mx-8 sm:py-3 rounded-full bg-black mt-60 sm:mt-80 align-bottom justify-center space-x-4">
+      <div className="flex flex-row mx-5 py-1 sm:mx-8 sm:py-3 rounded-full bg-black  align-bottom justify-center space-x-4">
       <button 
           className="px-4 py-2 text-white text-xl sm:text-2xl cursor-pointer"
-         >{backward}
+        onClick={handleBackward} >{backward}
         </button>
         <button 
           className="px-6 sm:px-10 py-2 text-white text-xl sm:text-2xl cursor-pointer"
@@ -53,7 +85,7 @@ export function QuranAudioPlayer() {
         </button>
         <button 
           className="px-4 py-2 text-white text-xl sm:text-2xl cursor-pointer"
-         >{forward}
+       onClick={handleForward}  >{forward}
         </button>
         {/* You can add more controls here */}
       </div>
