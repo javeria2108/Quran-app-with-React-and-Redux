31e902db9be7466f85705456f56f47bb3f5a14d2
javeria2108 <javeriazh@gmail.com>
2023-07-06 13:39:37 +0500

    w-full sm:w-11/12 md:w-10/12 lg:w-8/12 h-full sm:h-11/12 md:h-10/12 lg:h-8/12 bg-white rounded shadow p-5

--- src/Components/AudioPlayer.jsx
+++ src/Components/AudioPlayer.jsx
@@ -26,10 +26,12 @@ export function QuranAudioPlayer() {
 
 
     return (
-      <div className="flex justify-center items-center h-screen bg-gray-800 text-white">
+      <div className="flex justify-center items-center h-screen bg-gradient-to-b from-cyan-700 to-slate-950 text-white">
+     <div className="w-full sm:w-11/12 md:w-10/12 lg:w-8/12 h-3/4 sm:h-11/12 md:h-10/12 lg:h-3/4 bg-white rounded shadow p-5">
       <audio ref={audioRef} src={audioUrl} preload="metadata">
         Your browser does not support the audio element.
       </audio>
+      
       <div className="flex space-x-4">
         <button 
           className="px-4 py-2 bg-blue-500 text-white cursor-pointer"
@@ -37,6 +39,7 @@ export function QuranAudioPlayer() {
           {isPlaying ? 'Pause' : 'Play'}
         </button>
         {/* You can add more controls here */}
+      </div>
       </div>
     </div>
     )
